2e3dc25b1ca1eefca381f3e9f3a98233de3da066
Eugene Song <yougene42193@gmail.com>
2018-12-03 17:49:08 -0500

    added decode function

--- Jedi.js
+++ Jedi.js
@@ -20,3 +20,44 @@ function beyond(num) {
     return num;
 }
 beyond(100);
+
+function decode(uncoded) {
+    const newArr = [];
+    const splitArr = uncoded.split(" ");
+    for (let y in splitArr) {
+        if (splitArr[y][0] === "a") {
+            newArr.push(splitArr[y][1])
+        } else if (splitArr[y][0] === "b") {
+            newArr.push(splitArr[y][2])
+        } else if (splitArr[y][0] === "c") {
+            newArr.push(splitArr[y][3])
+        } else if (splitArr[y][0] === 'd') {
+            newArr.push(splitArr[y][4])
+        } else {
+            newArr.push(" ")
+        }
+    }
+    return newArr.join("");
+}
+console.log(decode(['craft', 'block']))
+/*function decode(letter) {
+    let word = letter.charAt(0);
+    if (word === 'a') {
+        return letter.charAt(1);
+    }
+    else if (word === 'b') {
+        return letter.charAt(2);
+    }
+    else if (word === 'c') {
+        return letter.charAt(3);
+    }
+    else if (word === 'd') {
+        return letter.charAt(4);
+    }
+    else {
+        return ' ';
+    }
+}
+
+console.log(decode('craft'))
+console.log(decode('block'))

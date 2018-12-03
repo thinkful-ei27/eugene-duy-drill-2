fc9da7f15c24738649a4877b73b13201105ac965
Eugene Song <yougene42193@gmail.com>
2018-12-03 17:16:56 -0500

    added function Beyond

--- Jedi.js
+++ Jedi.js
@@ -3,3 +3,20 @@ function jediName(firstName, lastName) {
 }  
 
 console.log(jediName('Beyonce', 'Knowles'))
+
+function beyond(num) {
+    if (num === Infinity || num === Number.NEGATIVE_INFINITY) {
+        console.log('And beyond');
+    }
+    else if (num > 0) {
+        console.log('To infinity');
+    }
+    else if (num < 0) {
+        console.log('To negative infinity');
+    } 
+    else {
+        console.log('Staying home');
+    }
+    return num;
+}
+beyond(100);
